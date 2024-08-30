5fa00a1d554cfcaa47e98944c41c4c389f19adb9
Gerson Muñoz <desarrolladorwebandnet5@gmail.com>
2024-08-30 17:13:56 -0500

    Adding files

---
+++ src/rmi/imc/Main.java
@@ -0,0 +1,13 @@
+package rmi.imc;
+
+import rmi.imc.views.MainView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainView view = new MainView();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+    
+}
